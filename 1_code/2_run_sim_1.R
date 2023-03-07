4f481de17f683ac4b88347587c8591fa02fa3567
Christopher Boyer <christopherbboyer@gmail.com>
2023-03-07 08:35:08 -0500

    update code

--- 1_code/2_run_sim_1.R
+++ 1_code/2_run_sim_1.R
@@ -1,133 +1,18 @@
-library(data.table)
-library(kableExtra)
-library(knitr)
-library(tidyverse)
-
-# function to generate data from simulation process
-gendata <- function(
-    N_train = 1000,  # number of training observations
-    N_test = 1000,   # number of observations in test set
-    N_truth = 1000,  # number of observations for calculating true/oracle MSE
-    sims = 1         # number of simulations
-  ) {
-  
-  N <- N_train + N_test + N_truth
-  X <- runif(N * sims, 0, 10)
-  A <- rbinom(N * sims, 1, plogis(-1.5 + 0.3 * X))
-  
-  split <- factor(x = rep(c(
-    rep("train", N_train),
-    rep("test", N_test),
-    rep("truth", N_truth)
-  ),
-  sims))
-    
-  Y <- 1 + X + 0.5 * X^2 - 3 * A * I(split != "truth") + rnorm(N * sims)
-  
-  dt <- data.table(
-    sim = rep(1:sims, each = N), 
-    split = split,
-    X = X,
-    A = A,
-    Y = Y
-  )
-  
-  setkey(dt, sim)
-  
-  return(dt)
-}
-
-# fit models to each sim
-runsim <- function(df) {
-  
-  # weight models
-  pA <-
-    lapply(c("train", "test"), function(x) {
-      fit <- glm(
-        A ~ X,
-        family = binomial(link = "logit"),
-        data = subset(df, split == x)
-      )
-      
-      predict(fit, 
-              newdata = subset(df, split == x & A == 0), 
-              type = "response")
-    })
-  
-  names(pA) <- c("train", "test")
-
-  # prediction models
-  models <-
-    list(
-      naive_miss = 
-        glm(
-          Y ~ X,
-          family = gaussian(link = "identity"),
-          data = subset(df, split == "train")
-        ),
-      naive_correct = 
-        glm(
-          Y ~ X + I(X^2),
-          family = gaussian(link = "identity"),
-          data = subset(df, split == "train")
-        ),
-      ipw_miss =
-        glm(
-          Y ~ X,
-          family = gaussian(link = "identity"),
-          weights = 1 / (1 - pA[['train']]),
-          data = subset(df, split == "train" & A == 0)
-        ),
-      ipw_correct =
-        glm(
-          Y ~ X + I(X^2),
-          family = gaussian(link = "identity"),
-          weights = 1 / (1 - pA[['train']]),
-          data = subset(df, split == "train" & A == 0)
-        )
-    )
-  
-  
-  # MSE 
-  unweighted <- lapply(models, function(fit) {
-    err <- df$Y[df$split == "test"] - 
-      predict(fit, newdata = subset(df, split == "test"))
-    
-    mean(err^2)
-  })
-  
-  weighted <- lapply(models, function(fit, pA) {
-    err <- df$Y[df$split == "test" & df$A == 0] - 
-      predict(fit, newdata = subset(df, split == "test" & A == 0))
-    
-    weighted.mean(err^2, 1 / (1 - pA[['test']]))
-  }, pA = pA)
-  
-  truth <- lapply(models, function(fit) {
-    err <- df$Y[df$split == "truth"] - 
-      predict(fit, newdata = subset(df, split == "truth"))
-    
-    mean(err^2)
-  })
-  
-  
-  return(c(unweighted, weighted, truth))
-}
 
 set.seed(8761276)
 
-dt <- gendata(sims = 10000) 
+dt1 <- gendata1(sims = 10000) 
 
-dt <- dt[, .(
-  mse = unlist(runsim(.SD)),
+dt1 <- dt1[, .(
+  mse = unlist(runsim1(.SD)),
   type = rep(c("unweighted", "weighted", "truth"), each = 4),
   fit = rep(c("OLS, miss", "OLS, cor", "WLS, miss", "WLS, cor"), 3)
 ), by = sim]
 
-final <- dt[, .(mse = mean(mse)), by = list(type, fit)]
+final1 <- dt1[, .(mse = mean(mse)), by = list(type, fit)]
 
-final_output <-
-  as_tibble(final) %>%
+final_output1 <-
+  as_tibble(final1) %>%
   pivot_wider(names_from = type, values_from = mse) %>%
   mutate(
     fit = str_replace(fit, "miss", "misspecified"),
@@ -135,7 +20,7 @@ final_output <-
   )
 
 kable(
-  x = final_output,
+  x = final_output1,
   format = "latex",
   col.names = c(
     "Estimator", 
--- 1_code/6_validate_app_models.R
+++ 1_code/6_validate_app_models.R
@@ -66,11 +66,17 @@ h_ipw <- pY - 2 * p_ipw * pY + p_ipw^2
 test <- 
   test |>
   mutate(
+    Y = cvda,
     w = (A * e[['num']] + (1 - A) * (1 - e[['num']])) / 
       (A * e[['den']] + (1 - A) * (1 - e[['den']]))
   )
 
-mse <- 
+auc_naive_naive <- cal_auc(test$cvda, p_naive)$auc
+auc_naive_ipw <- cal_auc(test$cvda, p_ipw)$auc
+aucs_naive <- cal_counter_auc(test, p_naive, e_fit$den, h_fit, se.fit = FALSE)
+aucs_ipw <- cal_counter_auc(test, p_ipw, e_fit$den, h_fit, se.fit = FALSE)
+
+results <- 
   test |>
   summarise(
     mse_naive_naive = mean((p_naive - cvda)^2),
@@ -82,24 +88,223 @@ mse <-
     mse_dr_naive = mean(I(A == 0) * w * ((p_naive - cvda)^2 - h_naive) + h_naive),
     mse_dr_ipw = mean(I(A == 0) * w * ((p_ipw - cvda)^2 - h_ipw) + h_ipw)
   ) |>
+  mutate(
+    auc_naive_naive = auc_naive_naive,
+    auc_naive_ipw = auc_naive_ipw,
+    auc_ipw_naive = aucs_naive$ipw,
+    auc_ipw_ipw = aucs_ipw$ipw,
+    auc_cl_naive = aucs_naive$om,
+    auc_cl_ipw = aucs_ipw$om,
+    auc_dr_naive = aucs_naive$dr,
+    auc_dr_ipw = aucs_ipw$dr
+  ) |>
   pivot_longer(
     everything(),
-    names_pattern = "(.*)_(.*)",
-    names_to = c(".value", "estimator")
-  ) 
+    names_pattern = "(.*)_(.*)_(.*)",
+    names_to = c(".value", "estimator", "model")
+  ) |>
+  pivot_wider(
+    names_from = model,
+    values_from = c(mse, auc)
+  )
 
 
 # bootstrap uncertainty ---------------------------------------------------
 
 replicates <- 1000
 
-boots <- rerun(replicates, bootfun(data = mesa))
+with_progress({
+  p <- progressor(steps = replicates)
+  
+  boots <- rerun(replicates, bootfun(data = mesa, p = p))
+})
 
 se <- 
   boots |>
   bind_rows(.id = "sim") |>
   group_by(estimator) |>
-  summarise(across(starts_with("mse"), sd)) 
+  summarise(across(-sim, sd)) 
 
-bind_rows(mse, se)
+application_results <- 
+  bind_rows(results, se) |>
+  arrange(match(estimator, c("naive", "cl", "ipw", "dr")), desc(mse_naive)) |>
+  group_by(estimator) |>
+  mutate(
+    estimator = case_when(
+      estimator == "naive" & row_number() == 1 ~ "Naive",
+      estimator == "cl" & row_number() == 1 ~ "CL",
+      estimator == "ipw" & row_number() == 1 ~ "IPW",
+      estimator == "dr" & row_number() == 1 ~ "DR",
+      TRUE ~ ""
+      ),
+    across(c(starts_with("mse"), starts_with("auc")), specd, k = 3),
+    across(c(starts_with("mse"), starts_with("auc")), as.character),
+    across(c(starts_with("mse"), starts_with("auc")), function(x)
+      if_else(row_number() == 2, paste0("(", x, ")"), x))
+  )
 
+kable(
+  x = application_results,
+  format = "latex",
+  col.names = c(
+    "Estimator",
+    "Logit", 
+    "Weighted Logit", 
+    "Logit", 
+    "Weighted Logit"
+  ),
+  digits = 3,
+  align = "lcccc",
+  escape = FALSE,
+  booktabs = TRUE
+) %>%
+  add_header_above(c(" " = 1, "MSE" = 2, "AUC" = 2)) |>
+  footnote(
+    threeparttable = TRUE,
+    general_title = "",
+    general = "The first column refers to the posited prediction model: the first model is an (unweighted) logistic regression model and the second is a logistic regression model with inverse probability weights for remaining statin-free. $\\\\widehat{\\\\psi}_{Naive}$ is the empirical estimator of the MSE using factual outcomes, $\\\\widehat{\\\\psi}_{CL}$ is the conditional loss estimator, $\\\\widehat{\\\\psi}_{IPW}$ is the inverse probability weighting estimator, $\\\\widehat{\\\\psi}_{DR}$ is the doubly-robust estimator. Standard error estimates are shown in parentheses obtained via 1000 bootstrap replicates.",
+    escape = FALSE
+  )
+
+
+
+# calibration
+test$p_naive <- p_naive
+test$p_ipw <- p_ipw
+
+test <- 
+  test |>
+  mutate(
+    Y = cvda,
+    w2 = (1 - A) * (1 - e[['num']])/(1 - e[['den']]) + 0
+  )
+
+loess.gcv <- function(x, y, w) {
+  nobs <- length(y)
+  tune.loess <- function(s) {
+    lo <- loess(y ~ x, weights = w, span = s)
+    mean((lo$fitted - y) ^ 2) / (1 - lo$trace.hat / nobs) ^ 2
+  }
+  os <- optimize(tune.loess, interval = c(.01, 99))$minimum
+  
+  loess(y ~ x, weights = w, span = os)
+}
+
+loess_naive <-
+  loess(cvda ~ p_naive, data = subset(test, A == 0), weights = w2)
+  # locfit(cvda ~ lp(p_naive, deg = 2), data = subset(test, A == 0), weights = w2, family = "huber")
+#   loess.gcv(
+#     x = test$p_naive[test$A == 0],
+#     y = test$cvda[test$A == 0],
+#     w = test$w2[test$A == 0]
+# )
+loess_ipw <-
+  loess(cvda ~ p_ipw, data = subset(test, A == 0), weights = w2)
+# locfit(cvda ~ lp(p_ipw, deg = 2), data = subset(test, A == 0), weights = w2, family = "huber")
+  # loess.gcv(
+  #   x = test$p_ipw[test$A == 0],
+  #   y = test$cvda[test$A == 0],
+  #   w = test$w2[test$A == 0]
+  # )
+
+calib <- subset(test, A == 0)
+# calib$curve_naive <- predict(loess_naive, newdata = calib, type = "response")
+# calib$curve_ipw <- predict(loess_ipw, newdata = calib, type = "response")
+
+calib$curve_naive <- predict(loess_naive, newdata = calib$p_naive, type = "response")
+calib$curve_ipw <- predict(loess_ipw, newdata = calib$p_ipw, type = "response")
+
+plt1 <- calib |>
+  select(p_naive, p_ipw, curve_naive, curve_ipw, cvda) |>
+  pivot_longer(
+    cols = c(p_naive, p_ipw, curve_naive, curve_ipw),
+    names_pattern = "(.*)_(.*)",
+    names_to = c('.value', 'model')
+  ) |>
+  filter(model == "naive")
+
+p1 <- 
+  ggplot(plt1, aes(x = p, y = curve),) + 
+  geom_line(linewidth = 1.25) +
+  geom_rug(sides = "b", alpha = 0.3, data = filter(plt1, cvda == 0), position = "jitter") +
+  geom_rug(sides = "t", alpha = 0.3, data = filter(plt1, cvda == 1), position = "jitter") +
+  geom_abline(slope = 1, linetype = "dashed") +
+  theme_bw(base_size = 10) +
+  theme(panel.grid = element_blank(), plot.title = element_text(hjust = 0.5)) +
+  scale_y_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  scale_x_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  labs(
+    title = "Logit",
+    x = "Predicted risk",
+    y = "Observed risk (weighted)"
+  )
+    
+
+plt2 <- calib |>
+  select(p_naive, p_ipw, curve_naive, curve_ipw, cvda) |>
+  pivot_longer(
+    cols = c(p_naive, p_ipw, curve_naive, curve_ipw),
+    names_pattern = "(.*)_(.*)",
+    names_to = c('.value', 'model')
+  ) |>
+  filter(model == "ipw")
+
+p2 <- 
+  ggplot(plt2, aes(x = p, y = curve)) + 
+  geom_line(linewidth = 1.25) +
+  geom_rug(sides = "b", alpha = 0.3, data = filter(plt2, cvda == 0)) +
+  geom_rug(sides = "t", alpha = 0.3, data = filter(plt2, cvda == 1)) +
+  geom_abline(slope = 1, linetype = "dashed") +
+  theme_bw(base_size = 10) +
+  theme(panel.grid = element_blank(), plot.title = element_text(hjust = 0.5)) +
+  scale_y_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  scale_x_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  labs(
+    title = "Weighted Logit",
+    x = "Predicted risk",
+    y = "Observed risk (weighted)"
+  )
+
+plt <- calib |>
+  select(p_naive, p_ipw, cvda) |>
+  pivot_longer(
+    cols = c(p_naive, p_ipw),
+    names_pattern = "(.*)_(.*)",
+    names_to = c('.value', 'model')
+  )
+
+plt3 <- filter(plt, model == "naive")
+
+p3 <- 
+  ggplot(plt3, aes(x = p)) + 
+  geom_histogram(aes(y = -1 * after_stat(count)), bins = 40, data = subset(plt3)) +
+  geom_histogram(bins = 40, data = subset(plt3, cvda == 1)) +
+  geom_hline(yintercept = 0) +
+  scale_x_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  theme_bw()  +
+  labs(
+    x = NULL,
+    y = ""
+  )
+  
+plt4 <- filter(plt, model == "ipw")
+
+p4 <- 
+  ggplot(plt3, aes(x = p)) + 
+  geom_histogram(aes(y = -1 * after_stat(count)), bins = 40, data = subset(plt4)) +
+  geom_histogram(bins = 40, data = subset(plt4, cvda == 1)) +
+  geom_hline(yintercept = 0) +
+  scale_x_continuous(breaks = seq(0, 0.75, 0.25), limits = c(-0.05, 0.85)) +
+  theme_bw() +
+  labs(
+    x = NULL,
+    y = ""
+  ) 
+  
+ggsave(filename = "3_figures/calib.pdf",
+       plot = (p1 + p2),
+       device = "pdf",
+       width = 6.5,
+       height = 3.5
+)
+       
